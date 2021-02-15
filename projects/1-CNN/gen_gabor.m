4231687d90569ef2b61d9eb32c8d0c74efbdb902
Bryan Conklin <6412103+thor4@users.noreply.github.com>
2021-02-15 11:09:16 -0500

    test generation of gabor patches

---
+++ projects/1-CNN/gen_gabor.m
@@ -0,0 +1,48 @@
+%% Generate gabor patches with labels to be used in training CNN model
+% exp code can be found here: OneDrive\Documents\postdoc @ Georgia Tech\projects\cnn\doby-exp\Expt1
+% example can be found in one_block.m, line 35:
+% gaborPatch = makeGaborPatch(p.stimSize, [], p.contrasts(contr_num), p.noiseContrast, .5, 'sd');
+% func args: makeGaborPatch(width,nGaussianSDs,contrastFraction,contrastNoise,gratingPeriod,gratingPeriodUnits,orientation,black,white)
+% to get example 'p' var: load('results11_10_50.mat')
+
+%match all aspects of gabors from experiment
+width = 169 ; % p.stimSize in pixels 
+nGaussianSDs = [] ; %default in exp (6)
+contrast = 0.45 ; %middle contrast from experiment, dropping other 2
+noise = .2 ; %same as exp
+gratingPeriod = 0.5 ; %same as exp
+gratingPeriodUnits = 'sd' ; %same as exp
+orientation = 45 ; %add to / subtract from this to get classes
+tilt = 2.26; %avg tilt used in exp
+counter_tilt = orientation + tilt; clock_tilt = orientation - tilt; 
+
+gaborPatch = uint8(makeGaborPatch(width,[],contrast,noise,...
+    gratingPeriod,gratingPeriodUnits,orientation));
+
+gaborPatch_counter = uint8(makeGaborPatch(width,[],contrast,noise,...
+    gratingPeriod,gratingPeriodUnits,counter_tilt));
+
+% gaborPatch_counter = uint8(makeGaborPatch(width,[],contrast,noise,...
+%     gratingPeriod,gratingPeriodUnits,60));
+
+gaborPatch_clock = uint8(makeGaborPatch(width,[],contrast,noise,...
+    gratingPeriod,gratingPeriodUnits,clock_tilt));
+
+% gaborPatch_clock = uint8(makeGaborPatch(width,[],contrast,noise,...
+%     gratingPeriod,gratingPeriodUnits,-60));
+
+figure(1), clf
+subplot(1,3,1)
+imshow(gaborPatch_counter)
+title('Counter')
+subplot(1,3,2)
+imshow(gaborPatch)
+title('Baseline')
+subplot(1,3,3)
+imshow(gaborPatch_clock)
+title('Clock')
+
+%something is up with the orientation change Farshad made in the
+%makeGaborPatch function. ie: when I choose 0 for orientation, it gives a
+%vertical grating, pi/2 gives a horizontal. counter at 47.26 looks the same
+%as 60. clock at 42.74 looks like -45
