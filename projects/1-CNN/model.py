dcda83a507bebcbf31b11d35e57690af63a4af37
Bryan Conklin <6412103+thor4@users.noreply.github.com>
2021-03-16 14:46:53 +0000

    begin loading images for transfer learning tut

--- projects/1-CNN/model.py
+++ projects/1-CNN/model.py
@@ -8,6 +8,36 @@ import os
 
 tf.__version__ #2.4.0
 
+#my own images for customizing the pretrained model:
+curr_dir = os.getcwd() #make sure I'm in CNN project folder
+curr_dir = pathlib.Path(curr_dir) #converts str path to os-appropriate format (windows/unix)
+len(list(curr_dir.glob('**/*.png'))) #locates all the gabor images, ** means this dir and & subdir's
+#100 gabors of each class + base & gabor in main dir = 202
+clock = list(curr_dir.glob('images/clock/*')) #stores list of all paths to gabors of class 'clockwise'
+PIL.Image.open(str(clock[0])) #convert 1st PosixPath in list to str then open with PIL.Image class
+
+#only needed for tutorial purposes to get images for customizing the pretrained model:
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+# '/home/jovyan/.keras/datasets/cats_and_dogs.zip'
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
+# '/home/jovyan/.keras/datasets/cats_and_dogs_filtered'
+train_dir = os.path.join(PATH, 'train')
+# '/home/jovyan/.keras/datasets/cats_and_dogs_filtered/train'
+# there is a 'cats' and 'dogs' folder in here with 1000 images a piece (2/3)
+validation_dir = os.path.join(PATH, 'validation')
+# '/home/jovyan/.keras/datasets/cats_and_dogs_filtered/validation'
+# there is a 'cats' and 'dogs' folder in here with 500 images a piece (1/3)
+
+BATCH_SIZE = 32
+IMG_SIZE = (160, 160)
+
+train_dataset = image_dataset_from_directory(train_dir,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE)
+
 #load images
 path = os.getcwd() #make sure I'm in CNN project folder
 
