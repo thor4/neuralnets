b6966d1c6b54e5c78dd29292a3bbc656c7267ad4
Bryan Conklin <6412103+thor4@users.noreply.github.com>
2021-03-24 11:45:26 -0400

    update gabors to contrast 1 size 100

--- projects/1-CNN/gen_gabor.m
+++ projects/1-CNN/gen_gabor.m
@@ -8,25 +8,43 @@
 %match all aspects of gabors from experiment
 width = 169 ; % p.stimSize in pixels 
 nGaussianSDs = [] ; %default in exp (6)
-contrast = 0.45 ; %middle contrast from experiment, dropping other 2
-noise = 1 ; %same as exp
-gratingPeriod = 0.5 ; %same as exp
-gratingPeriodUnits = 'sd' ; %same as exp
-orientation = pi/4 ; %radians (45 deg), add to / subtract from this to get classes
+%contrasts = [.3, .45, 1]  # 30%, 45% and 100% contrast
+contrast = 1; %middle contrast from experiment, dropping other 2
+noise = 1; %same as exp
+gratingPeriod = 0.5; %same as exp
+gratingPeriodUnits = 'sd'; %same as exp
+orientation = pi/4; %radians (45 deg), add to / subtract from this to get classes
 tilt = 2.26*(pi/180); %avg tilt used in exp, degrees->radians: *(pi/180)
+%p.tilts = [middle_tilt/2, middle_tilt, 2*middle_tilt];
+%Middle tilt = 2.26 # supposed to yield 75% accuracy, this is in degrees
 counter_tilt = orientation - tilt; clock_tilt = orientation + tilt; 
 num_gabors = 100; %define total number of gabors to make for each class
+n_train_gabor = floor(num_gabors*(2/3)); %number of training gabor images
+n_valid_gabor = floor(0.8*(num_gabors-n_train_gabor)); %number of validation gabor images
+n_test_gabor = num_gabors - n_train_gabor - n_valid_gabor;
 
 %generate and save gabor images of size width+1 x width+1 (170x170 .png's)
 for i=1:num_gabors
     gaborPatch_counter = uint8(makeGaborPatch(width,[],contrast,noise,...
         gratingPeriod,gratingPeriodUnits,counter_tilt)); %counter clockwise
-    counter_label = sprintf('C:\\Users\\bryan\\Documents\\GitHub\\neuralnets\\projects\\1-CNN\\images\\counter\\counter%d.png',i);
-    imwrite(gaborPatch_counter,counter_label);
     gaborPatch_clock = uint8(makeGaborPatch(width,[],contrast,noise,...
         gratingPeriod,gratingPeriodUnits,clock_tilt)); %clockwise
-    clock_label = sprintf('C:\\Users\\bryan\\Documents\\GitHub\\neuralnets\\projects\\1-CNN\\images\\clock\\clock%d.png',i);
-    imwrite(gaborPatch_clock,clock_label);
+    if (ismember(i,(1:n_train_gabor)))
+        counter_label = sprintf('C:\\Users\\bryan\\Documents\\GitHub\\neuralnets\\projects\\1-CNN\\images\\train\\cclock\\cclock%d.png',i);
+        imwrite(gaborPatch_counter,counter_label);
+        clock_label = sprintf('C:\\Users\\bryan\\Documents\\GitHub\\neuralnets\\projects\\1-CNN\\images\\train\\clock\\clock%d.png',i);
+        imwrite(gaborPatch_clock,clock_label);
+    elseif (ismember(i,(n_train_gabor+1:n_train_gabor+n_valid_gabor))) 
+        counter_label = sprintf('C:\\Users\\bryan\\Documents\\GitHub\\neuralnets\\projects\\1-CNN\\images\\validation\\cclock\\cclock%d.png',i);
+        imwrite(gaborPatch_counter,counter_label);
+        clock_label = sprintf('C:\\Users\\bryan\\Documents\\GitHub\\neuralnets\\projects\\1-CNN\\images\\validation\\clock\\clock%d.png',i);
+        imwrite(gaborPatch_clock,clock_label);
+    else
+        counter_label = sprintf('C:\\Users\\bryan\\Documents\\GitHub\\neuralnets\\projects\\1-CNN\\images\\test\\cclock\\cclock%d.png',i);
+        imwrite(gaborPatch_counter,counter_label);
+        clock_label = sprintf('C:\\Users\\bryan\\Documents\\GitHub\\neuralnets\\projects\\1-CNN\\images\\test\\clock\\clock%d.png',i);
+        imwrite(gaborPatch_clock,clock_label);
+    end
 end
 
 %create  base case (45 deg) and visualize difference
