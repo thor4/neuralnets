27098f9bc1d1b9c33fe309c88c5183ef25bdc79d
Bryan Conklin <6412103+thor4@users.noreply.github.com>
2022-03-18 14:47:54 -0400

    gen cifar10 qualifying tilt corpus

--- projects/1-CNN/gen_gabor-range_rand-test_sets.m
+++ projects/1-CNN/gen_gabor-range_rand-test_sets.m
@@ -13,8 +13,8 @@
 %match all aspects of gabors from experiment
 width = 169 ; % p.stimSize in pixels 
 nGaussianSDs = [] ; %default in exp (6)
-% contrasts = [0.3, .45, 1];  % 30%, 45% and 100% contrast
-contrast = 0.45; %middle contrast from experiment, dropping other 2
+contrasts = [0.3, .45, 1];  % 30%, 45% and 100% contrast, qualifying tilt/contrast corpus
+% contrast = 0.45; %middle contrast from experiment, dropping other 2
 noise = 1; %same as exp
 gratingPeriod = 0.5; %same as exp
 gratingPeriodUnits = 'sd'; %same as exp
@@ -38,45 +38,49 @@ rng(0,'twister'); %init random number generator to make results repeatable
 %Middle tilt = 2.26 # supposed to yield 75% accuracy, this is in degrees
 % counter_tilts = orientation - tilts; clock_tilts = orientation + tilts; 
 % tilts = [0.05:13/160:2] .* (pi/180); %25 tilts tiled evenly from 0.05-2, converted to radians
-tilts = [0.5:3.5/24:4] .* (pi/180); %25 tilts tiled evenly from 0.5-4, converted to radians
+% tilts = [0.5:3.5/24:4] .* (pi/180); %25 tilts tiled evenly from 0.5-4, converted to radians
 % tilts = [0.05:13/160:0.05+(13/160)*2] .* (pi/180); %3 tilts for van gabor
-fnames = ["s1","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","s12",...
-    "s13","s14","s15","s16","s17","s18","s19","s20","s21","s22","s23",...
-    "s24","s25"];
-% fnames = "%s-%s"; %qualifying tilt/contrast corpus
-% contrast_names = ["c_0_3","c_0_45","c_1"];
+tilts = [0.5+(3.5/24*4) 0.5+(3.5/24*13) 4] .* (pi/180); %3 tilts for cifar10
+% fnames = ["s1","s2","s3","s4","s5","s6","s7","s8","s9","s10","s11","s12",...
+%     "s13","s14","s15","s16","s17","s18","s19","s20","s21","s22","s23",...
+%     "s24","s25"];
+fnames = "%s-%s"; %qualifying tilt/contrast corpus
+contrast_names = ["c_0_3","c_0_45","c_1"]; %qualifying tilt/contrast corpus
+tilt_names = ["t_1_0833","t_2_3958","t_4"]; %cifar10 tilts
 % tilt_names = ["t_0_05","t_0_1313","t_0_2125"]; %van gabor tilts
 % root_dir_clock = "J:\\OneDrive - Georgia Institute of Technology\\projects\\metacognitive bias\\stimuli\\testing\\tilt_contrast-van_gabor\\%s\\clock\\clock%d.png";
 % root_dir_cclock = "J:\\OneDrive - Georgia Institute of Technology\\projects\\metacognitive bias\\stimuli\\testing\\tilt_contrast-van_gabor\\%s\\cclock\\cclock%d.png";
-root_dir_clock = "J:\\OneDrive - Georgia Institute of Technology\\projects\\metacognitive bias\\stimuli\\testing\\tilt_0_5-4_contrast_0_45\\%s\\clock\\clock%d.png";
-root_dir_cclock = "J:\\OneDrive - Georgia Institute of Technology\\projects\\metacognitive bias\\stimuli\\testing\\tilt_0_5-4_contrast_0_45\\%s\\cclock\\cclock%d.png";
+root_dir_clock = "J:\\OneDrive - Georgia Institute of Technology\\projects\\metacognitive bias\\stimuli\\testing\\tilt_contrast-cifar10\\%s\\clock\\clock%d.png";
+root_dir_cclock = "J:\\OneDrive - Georgia Institute of Technology\\projects\\metacognitive bias\\stimuli\\testing\\tilt_contrast-cifar10\\%s\\cclock\\cclock%d.png";
+% root_dir_clock = "J:\\OneDrive - Georgia Institute of Technology\\projects\\metacognitive bias\\stimuli\\testing\\tilt_0_5-4_contrast_0_45\\%s\\clock\\clock%d.png";
+% root_dir_cclock = "J:\\OneDrive - Georgia Institute of Technology\\projects\\metacognitive bias\\stimuli\\testing\\tilt_0_5-4_contrast_0_45\\%s\\cclock\\cclock%d.png";
 num_gabors = 2000; %define number of gabors to make for each contrast
 % 3500 gabors * 3 contrasts * 2 classes = 21,000 gabors
 imageN = 1; %initialize image counter
-%UPDATE WHERE IMAGES WILL BE STORED TO SAVE ON OSF
-% tic %generate qualifying tilt/contrast corpus
-% for contrastN = 1:length(contrasts)
-%     contrast = contrasts(contrastN);
-%     contrast_name = contrast_names(contrastN);
-%     for tiltN = 1:length(tilts)
-%         counter_tilt = orientation - tilts(tiltN); 
-%         clock_tilt = orientation + tilts(tiltN); 
-%         fname = sprintf(fnames,tilt_names(tiltN),contrast_names(contrastN));
-%         imageN = gen_gabor(num_gabors,contrast,clock_tilt,width,noise,...
-%             gratingPeriod,gratingPeriodUnits,imageN,root_dir_clock,fname);
-%         imageN = gen_gabor(num_gabors,contrast,counter_tilt,width,noise,...
-%             gratingPeriod,gratingPeriodUnits,imageN,root_dir_cclock,fname);
-%     end
-% end
-% toc
-tic %generate test sets
-for tiltN = 1:length(tilts)
-    counter_tilt = orientation - tilts(tiltN); 
-    clock_tilt = orientation + tilts(tiltN); 
-    fname = fnames(tiltN);
-    imageN = gen_gabor(num_gabors,contrast,clock_tilt,width,noise,...
-        gratingPeriod,gratingPeriodUnits,imageN,root_dir_clock,fname);
-    imageN = gen_gabor(num_gabors,contrast,counter_tilt,width,noise,...
-        gratingPeriod,gratingPeriodUnits,imageN,root_dir_cclock,fname);
+
+tic %generate qualifying tilt/contrast corpus
+for contrastN = 1:length(contrasts)
+    contrast = contrasts(contrastN);
+    contrast_name = contrast_names(contrastN);
+    for tiltN = 1:length(tilts)
+        counter_tilt = orientation - tilts(tiltN); 
+        clock_tilt = orientation + tilts(tiltN); 
+        fname = sprintf(fnames,tilt_names(tiltN),contrast_names(contrastN));
+        imageN = gen_gabor(num_gabors,contrast,clock_tilt,width,noise,...
+            gratingPeriod,gratingPeriodUnits,imageN,root_dir_clock,fname);
+        imageN = gen_gabor(num_gabors,contrast,counter_tilt,width,noise,...
+            gratingPeriod,gratingPeriodUnits,imageN,root_dir_cclock,fname);
+    end
 end
 toc
+% tic %generate test sets
+% for tiltN = 1:length(tilts)
+%     counter_tilt = orientation - tilts(tiltN); 
+%     clock_tilt = orientation + tilts(tiltN); 
+%     fname = fnames(tiltN);
+%     imageN = gen_gabor(num_gabors,contrast,clock_tilt,width,noise,...
+%         gratingPeriod,gratingPeriodUnits,imageN,root_dir_clock,fname);
+%     imageN = gen_gabor(num_gabors,contrast,counter_tilt,width,noise,...
+%         gratingPeriod,gratingPeriodUnits,imageN,root_dir_cclock,fname);
+% end
+% toc
